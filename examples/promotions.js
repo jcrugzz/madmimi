abfadfcc4e4ca51402bb3978008fb4d3738339ad
Nicholas Young <nicholas@nicholaswyoung.com>
2014-11-26 17:17:34 -0600

    Amend promotions example.

--- examples/promotions.js
+++ examples/promotions.js
@@ -5,10 +5,35 @@ madmimi.configure({
   key: 'xxxx'
 });
 
+/**
+ * Get all promotions.
+ */
 madmimi.get('/promotions')
 .then(function (promotions) {
   console.log(promotions);
 })
 .error(function (err) {
   console.error(err);
+});
+
+/**
+ * Get a specific promotion.
+ */
+madmimi.get('/promotions/:id', {
+  params: {
+    id: '111'
+  }
+})
+.then(function (promotions) {
+  console.log(promotions);
+})
+.error(function (err) {
+  console.error(err);
+});
+
+/**
+ * If you prefer, there's a callback API as well.
+ */
+madmimi.get('/promotions', function (err, result) {
+
 });
